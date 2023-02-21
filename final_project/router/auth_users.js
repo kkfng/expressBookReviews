5c0ef2ba608b40ad8071d8e872eb4c0ef77030c9
Nowinski4 <124801366+Nowinski4@users.noreply.github.com>
2023-02-21 14:37:55 +0000

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,48 +3,89 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
+
+
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-
   const username = req.body.username;
   const password = req.body.password;
 
   if (!username || !password) {
-      return res.status(404).json({ message: "Error logging in" });
+      return res.status(404).json({message: "Error logging in"});
   }
 
-  if (authenticatedUser(username, password)) {
-      let accessToken = jwt.sign({
-        data: password,
-  }, 'access', { expiresIn: 60 * 60 });
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
 
-      req.session.authorization = {
-          accessToken, username
+    req.session.authorization = {
+      accessToken,username
   }
   return res.status(200).send("User successfully logged in");
   } else {
-    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
   }
-
 });
+
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review/:isbn", function (req, res) {
+    //Write your code here
+    const isbn = req.params.isbn + "";
+    const review = req.query.review;
+    const username = req.user.data;
+    const book = books[isbn];
+    if (book) {
+        book.reviews[username] = review;
+        return res.status(200).json(book);
+    }
+    return res.status(404).json({ message: "Invalid ISBN" });
 });
-
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
+  
+  // Delete book review
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+   //*Write your code here
+   const isbn = req.params.isbn + "";
+   const username = req.user.data;
+   const book = books[isbn];
+   if (book) {
+       delete book.reviews[username];
+       return res.status(200).json(book);
+   }
+   return res.status(404).json({ message: "Invalid ISBN" });
+});
+  
+  module.exports.authenticated = regd_users;
+  module.exports.isValid = isValid;
+  module.exports.users = users;
+  module.exports.authenticatedUser = authenticatedUser;
